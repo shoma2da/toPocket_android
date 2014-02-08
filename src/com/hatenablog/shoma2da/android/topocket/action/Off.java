62f64dd1ad7c2c32f40c2bffac4eef1bf2d46b2f
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-02-08 21:56:48 +0900

    アクセス修飾子を変更

--- src/com/hatenablog/shoma2da/android/topocket/action/Off.java
+++ src/com/hatenablog/shoma2da/android/topocket/action/Off.java
@@ -2,7 +2,7 @@ package com.hatenablog.shoma2da.android.topocket.action;
 
 import android.util.Log;
 
-public class Off implements SwitchActionStrategy {
+class Off implements SwitchActionStrategy {
 
     @Override
     public void act() {
--- src/com/hatenablog/shoma2da/android/topocket/action/On.java
+++ src/com/hatenablog/shoma2da/android/topocket/action/On.java
@@ -2,7 +2,7 @@ package com.hatenablog.shoma2da.android.topocket.action;
 
 import android.util.Log;
 
-public class On implements SwitchActionStrategy {
+class On implements SwitchActionStrategy {
 
     @Override
     public void act() {
