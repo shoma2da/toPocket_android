a78f23fe07059eec1e79fac75c9290a124a8bb97
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-02-08 20:28:17 +0900

    チェックボックスのイベントを仮作成

--- src/com/hatenablog/shoma2da/android/topocket/MainActivity.java
+++ src/com/hatenablog/shoma2da/android/topocket/MainActivity.java
@@ -2,6 +2,10 @@ package com.hatenablog.shoma2da.android.topocket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        CheckBox startWatchClipboardSwitch = (CheckBox)findViewById(R.id.StartWatchClipboardSwitch);
+        startWatchClipboardSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d("test", "checked change");
+            }
+        });
     }
 
 }
