e55e4daf5eafeaa47ee9d55f8154a52b7dd032a2
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-02-09 00:03:23 +0900

    チェック状態の保存

--- src/com/hatenablog/shoma2da/android/topocket/MainActivity.java
+++ src/com/hatenablog/shoma2da/android/topocket/MainActivity.java
@@ -1,6 +1,8 @@
 package com.hatenablog.shoma2da.android.topocket;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.CheckBox;
 
@@ -11,7 +13,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //チェックボタンの初期表示設定
         CheckBox startWatchClipboardSwitch = (CheckBox)findViewById(R.id.StartWatchClipboardSwitch);
+        SharedPreferences preferences = getSharedPreferences(SwitchListener.FILE_CHECKED_STATE, Context.MODE_PRIVATE);
+        boolean isChecked = preferences.getBoolean(SwitchListener.KEY_VALUE, false);
+        startWatchClipboardSwitch.setChecked(isChecked);
+        
+        //チェック時の動作を規定
         startWatchClipboardSwitch.setOnCheckedChangeListener(new SwitchListener(getLoaderManager()));
     }
 
--- src/com/hatenablog/shoma2da/android/topocket/SwitchListener.java
+++ src/com/hatenablog/shoma2da/android/topocket/SwitchListener.java
@@ -3,10 +3,15 @@ package com.hatenablog.shoma2da.android.topocket;
 import com.hatenablog.shoma2da.android.topocket.action.SwitchActionStrategyFactory;
 
 import android.app.LoaderManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class SwitchListener implements OnCheckedChangeListener {
+    
+    public static final String FILE_CHECKED_STATE = "checked_state";
+    public static final String KEY_VALUE = "value";
     
     private LoaderManager mLoaderManager;
     
@@ -16,6 +21,12 @@ public class SwitchListener implements OnCheckedChangeListener {
     
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        //チェック状態の保存
+        Context context = buttonView.getContext();
+        SharedPreferences preferences = context.getSharedPreferences(FILE_CHECKED_STATE, Context.MODE_PRIVATE);
+        preferences.edit().putBoolean(KEY_VALUE, isChecked).commit();
+        
+        //チェックに応じた処理を開始
         createFactory().create(buttonView.getContext(), mLoaderManager, isChecked).act();
     }
     
