4801d16f7a959db59bad0eea9d27b828f6cbdff2
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-02-08 19:14:03 +0900

    initial commit

---
+++ gen/com/hatenablog/shoma2da/android/topocket/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hatenablog.shoma2da.android.topocket;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
